93d9c61aa601e6ecf0dcbe9b118045ccc15fec31
PabloBaiocchi <pablo.b.baiocchi@gmail.com>
2022-10-22 21:06:41 -0300

    data visualization

--- dataAcquisition.py
+++ dataAcquisition.py
@@ -8,7 +8,9 @@ uniswap_endpoint='https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3'
 def getStablePoolHistory(min_tvl):
     stable_pools=getStablePools(min_tvl)
     df_list=[getPoolHistory(pool_id) for pool_id in stable_pools.pool_id]
-    return pd.concat(df_list).reset_index(drop=True)
+    history=pd.concat(df_list).reset_index(drop=True)
+    name_df=poolNames(stable_pools,history)
+    return history.merge(name_df[['pool_id','pool_name']],on='pool_id')
 
 def getStablePools(min_tvl):
     q=Query('./queries/stable_pair_pools')
@@ -34,6 +36,13 @@ def getPoolHistory(pool_id):
     df['pool_id']=pool_id
     return df
 
+def poolNames(stable_pools,history):
+    fee_df=history.groupby('pool_id').apply(fees).to_frame()
+    fee_df.columns=['fee']
+    name_df=fee_df.merge(stable_pools[['token0','token1','pool_id']],on='pool_id')
+    name_df['pool_name']=name_df.apply(lambda row: f'{row.token0}_{row.token1}_{row.fee}',axis=1)
+    return name_df
+
 def cleanPoolResponse(raw):
     return {
         'token0':raw['token0']['symbol'],
@@ -41,3 +50,6 @@ def cleanPoolResponse(raw):
         'tvl_usd':float(raw['totalValueLockedUSD']),
         'pool_id':raw['id']
     }
+
+def fees(df):
+    return str(round(df.feesUSD.sum()/df.volumeUSD.sum(),5))[-2:]
